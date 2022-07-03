07fec25c43135e345d3cf0075283e9da8db407d5
Fernando Serra <fs.serra@outlook.com>
2022-07-03 18:54:26 -0300

    minor alterations

--- cSDNG/specialEvent.c
+++ cSDNG/specialEvent.c
@@ -4,4 +4,8 @@
 int specialEvent(){
 	printf("\n\t\t Meio sem saber o que fazer, voce decide ficar ali mesmo. Depois de um tempo acha melhor retornar ao acampamento e acender a fogueira.");
 	printf("\n\t\t Talvez o velho volte a aparecer novamente.");
+	
+	system("PAUSE");
+	
+	printf("A noite chega mansa, o vento nos galhos traz um cheiro de fuligem e carne assada bem distante, voce nao sabe se esta realmente sentindo esses odores, ou se e seu estomago lhe fazendo imaginar coisas.");
 }
