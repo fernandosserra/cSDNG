2969c47e41a2954ec81744655babe6932ec66a74
Fernando Serra <fs.serra@outlook.com>
2024-06-14 13:28:05 -0300

    Feitos testes com setlocale, mas sem sucesso.

--- cSDNG/main.c
+++ cSDNG/main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <locale.h>
+// Bibliotecas Personalizadas / Personal Libs
 #include "selector.h"
 #include "classes.h"
 
 //Bloco principal de execucao / Main execution block
 int main(int argc, char *argv[]) {
+	setlocale(LC_ALL,"Portuguese");
 	int selection = 0;
 	int selectedLanguage = 0;
 	printf("\n\t\tHello Adventurer! Welcome to C Single Dungeon");
 	printf("\n\t\tType 1 for Portuguese, 2 for English:\n\t\t");
 	scanf("%d", &selection);
 	if (selection == 1) {
-		printf("\n\t\tVoce escolheu jogar em Portugues");
+		printf("\n\t\tVocê escolheu jogar em Português");
 		selectedLanguage = lang_PtBR();
 	} else if (selection == 2) {
 		printf("\n\t\tYou have chosen to play in English");
 		selectedLanguage = lang_EnUS();
 	} else {
-		printf("\n\t\tDigite uma opcao valida! / Choose a valid option!");
+		printf("\n\t\tDigite uma opção válida! / Choose a valid option!");
 	}
 	while (selection > 2 || selection < 1) {
 		printf("\n\t\tType 1 for Portuguese, 2 for English:\n\t\t");
@@ -24,4 +27,3 @@ int main(int argc, char *argv[]) {
 	}
 	return 0;
 }
-
